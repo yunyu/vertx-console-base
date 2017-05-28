7171e8ec03345ad1192d5b9edfab789837625a39
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-05-28 10:46:56 -0600

    Add custom tooltip support for sparkline

--- src/components/c3/SparklineChart.vue
+++ src/components/c3/SparklineChart.vue
@@ -3,15 +3,24 @@ import C3Wrapper from './C3Wrapper.vue';
 
 export default {
     name: 'pf-sparkline',
+    props: {
+        tooltipContents: {
+            type: Function
+        }
+    },
     extends: C3Wrapper,
     methods: {
         getDefaults(chartDefaults) {
-            return chartDefaults().getDefaultSparklineConfig();
+            var chartData = chartDefaults().getDefaultSparklineConfig();
+            if (this.tooltipContents) {
+                chartData.tooltip = this.tooltipContents;
+            }
+            return chartData;
         },
         getC3Data() {
             return {
                 columns: [
-                    [this.data.units].concat(this.data.values)
+                    this.data.units ? [this.data.units].concat(this.data.values) : this.data.values
                 ],
                 type: 'area'
             };
