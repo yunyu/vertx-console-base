cffbad293f059ba4dffba2bb39f022d68df47cad
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-05-29 09:42:39 -0600

    Add FDs

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -27,6 +27,7 @@
                 <div :class="getColumnClass(1)">
                     <pf-card class="match-util-trend" title="Usage Statistics" :accented="false" :showTitlesSeparator="false">
                         <pf-utilization-bar-chart title='Workers' units='threads' :value="parseInt(getSimpleMetricValue('vertx_pools_worker_vert_x_worker_thread_in_use'))" :total="parseInt(getSimpleMetricValue('vertx_pools_worker_vert_x_worker_thread_max_pool_size'))" inline :warning="60" :error="85"></pf-utilization-bar-chart>
+                        <pf-utilization-bar-chart title='Open Files' units='FDs' :value="parseInt(getSimpleMetricValue('process_open_fds'))" :total="parseInt(getSimpleMetricValue('process_max_fds'))" inline :warning="60" :error="85"></pf-utilization-bar-chart>    
                     </pf-card>
                 </div>
                 <div :class="getColumnClass(2)">
@@ -48,6 +49,7 @@
     margin-left: 10px;
     margin-right: 10px;
 }
+
 
 
 
