a34ab7464e645dad39f7fcde36e51741ea3af20e
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-05-30 13:29:48 -0600

    hmm

--- src/components/c3/FlowedLine.vue
+++ src/components/c3/FlowedLine.vue
@@ -23,11 +23,18 @@ export default {
             if (!this.chart || !this.chartData) {
                 return;
             }
+
+            this.buffer.push(this.chartData.data);
+            while (this.buffer.length > this.maxDisplayed) {
+                this.buffer.shift();
+            }
+
             const origDuration = this.chart.internal.config.transition_duration;
             if (document.hidden) {
                 this.chart.internal.config.transition_duration = 0;
                 this.chartData.data.duration = 0;
             }
+
             this.chart.flow(this.chartData.data);
             this.chart.internal.config.transition_duration = origDuration;
         }
