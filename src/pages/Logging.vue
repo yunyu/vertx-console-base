560ef0ad31bd515d988f2ea7140496b117e1cdfe
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-06-12 10:04:21 -0600

    add headers

--- src/pages/Logging.vue
+++ src/pages/Logging.vue
@@ -1,11 +1,76 @@
 <template>
-<div></div>
+    <div>
+        <div class="page-header">
+            <h1>Logging</h1>
+        </div>
+        <div class="log-header">
+            <div class="logger-name">{{ logger }}</div>
+            <div class="log-level">{{ logLevel }}</div>
+        </div>
+        <div class="log-display" id="logDisplay">
+            <div class="log-line" v-for="logElement in logMsgs">[{{ dateFormat(logElement.date, 'HH:MM:ss') }}] [{{ logElement.level }}] {{ logElement.thread }} - {{ logElement.message }}</div>
+        </div>
+    </div>
 </template>
 
+<style lang="scss">
+.log-display {
+    background: black;
+    color: #ccc;
+    font-family: monospace;
+    padding: 20px;
+    height: 500px;
+    overflow-y: scroll;
+}
+
+.log-header {
+    overflow: hidden;
+    padding: 10px;
+    border: 1px solid #ddd;
+    background-clip: padding-box;
+    background-color: #f5f5f5;
+    background-image: linear-gradient(to bottom,#fafafa 0,#ededed 100%);
+    background-repeat: repeat-x;
+}
+
+.logger-name, .log-level {
+    display: inline-block;
+    font-size: 16px;
+}
+
+.logger-name {
+    float: left;
+    font-weight: bold;
+}
+
+.log-level {
+    float: right;
+}
+</style>
+
+
 <script>
+import EventBus from 'vertx-eventbus';
+import dateFormat from 'dateformat';
+
 export default {
     mounted() {
-        
+        this.eb = new EventBus('http://twitterwall.yunyul.in/loggerproxy/'); // wtf webpack isn't proxying this properly
+        setTimeout(() => {
+            this.eb.registerHandler("vertx.console.logger.default", (e, m) => {
+                this.logMsgs.push(JSON.parse(m.body));
+                var container = this.$el.querySelector("#logDisplay");
+                container.scrollTop = container.scrollHeight;
+            })
+        }, 1000);
+    },
+    data() {
+        return {
+            logMsgs: [],
+            logger: "Default",
+            logLevel: "INFO",
+            dateFormat: dateFormat
+        }
     }
 }
 </script>
