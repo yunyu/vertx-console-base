6da7c6a58714d562a091743b6c0325e4bace678e
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-06-12 08:42:15 -0600

    Css tweaks

--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -1,13 +1,30 @@
 <template>
     <div>
+        <div class="page-header">
+            <h1>Services</h1>
+        </div>
         <datatable :columns="tableColumns" :data="tableRows" filterable paginate></datatable>
     </div>
 </template>
 
 <style lang="scss">
-    .table thead .glyphicon {
-        color: #aaa;
-    }
+.table thead .glyphicon {
+    color: #aaa;
+}
+
+.table-above .col-xs-12, .table-below .col-xs-12 {
+    padding-left: 6px;
+    padding-bottom: 16px;
+}
+
+table.table {
+    border: 1px solid #ddd;
+    background: white;
+}
+
+.table-below .btn-group {
+    vertical-align: top;
+}
 </style>
 
 <script>
