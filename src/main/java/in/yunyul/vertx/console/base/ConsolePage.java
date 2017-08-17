047b1af8d2d047c9c31a0cd57b462f4dbb03712b
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-08-17 08:51:40 -0500

    Add ConsolePage docs

--- src/main/java/in/yunyul/vertx/console/base/ConsolePage.java
+++ src/main/java/in/yunyul/vertx/console/base/ConsolePage.java
@@ -4,7 +4,21 @@ import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
 
 public interface ConsolePage {
+    /**
+     * Sets up and mounts the supporting endpoints for the page.
+     *
+     * @param vertx    Vert.x instance to use
+     * @param router   Router to mount endpoints on
+     * @param basePath Base path to mount the endpoints under
+     */
     void mount(Vertx vertx, Router router, String basePath);
 
+    /**
+     * Provides the file name of the script to inject on the client,
+     * relative to {@link WebConsoleRegistry#CONSOLE_ROOT}.
+     * Should start with a forward slash, e.g. '/js/mypage.js'.
+     *
+     * @return The script file name
+     */
     String getLoaderFileName();
 }
